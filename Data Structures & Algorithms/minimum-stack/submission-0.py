5bbddae4118a6d8374b35b5c5f7bd963ed6120c4
Sonya Kim <sonyakim.dev@gmail.com>
2026-05-12 19:04:14 -0700

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,20 @@
+class MinStack:
+
+    def __init__(self):
+        self.stk = []
+        self.min_stk = []
+
+    def push(self, val: int) -> None:
+        self.stk.append(val)
+        m = min(val, self.min_stk[-1] if self.min_stk else val)
+        self.min_stk.append(m)
+
+    def pop(self) -> None:
+        self.stk.pop()
+        self.min_stk.pop()
+
+    def top(self) -> int:
+        return self.stk[-1]
+
+    def getMin(self) -> int:
+        return self.min_stk[-1]
