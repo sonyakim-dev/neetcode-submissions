e338c77db39984d63d058b8a67b71772cd509a61
Sonya Kim <sonyakim.dev@gmail.com>
2026-05-17 18:41:12 -0700

    Add: search-for-word - submission-3

---
+++ Data Structures & Algorithms/search-for-word/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        direction = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        row, col = len(board), len(board[0])
+        visited = set()
+
+        def dfs(r, c, i):
+            if i == len(word): return True
+            if r not in range(row) or c not in range(col)\
+                or board[r][c] != word[i] or (r, c) in visited:
+                return False
+
+            visited.add((r, c))
+            for dr, dc in direction:
+                if dfs(dr + r, dc + c, i + 1): return True
+            visited.remove((r, c))
+            return False
+
+        for r in range(row):
+            for c in range(col):
+                if dfs(r, c, 0): return True
+        return False
