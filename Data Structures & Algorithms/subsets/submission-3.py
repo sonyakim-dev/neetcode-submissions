4bd160dce6e7964a8be9e191246bd51e2b409631
Sonya Kim <sonyakim.dev@gmail.com>
2026-05-21 17:27:55 -0700

    Add: subsets - submission-3

---
+++ Data Structures & Algorithms/subsets/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        """
+        BT problem. iterate each nums and decide either include it or not
+        """
+        # result = []
+        # subset = []
+
+        # def dfs(i):
+        #     if i >= len(nums):
+        #         result.append(subset.copy())
+        #         return
+
+        #     subset.append(nums[i])
+        #     dfs(i+1)
+
+        #     subset.pop()
+        #     dfs(i+1)
+
+        # dfs(0)
+        # return result
+
+        result = [[]]
+        for n in nums:
+            result += [el + [n] for el in result]
+        return result
