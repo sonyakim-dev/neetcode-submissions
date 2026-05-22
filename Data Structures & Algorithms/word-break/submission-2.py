d35dfea2722b65ba9d02585e7ff2c4ccb03c1dae
Sonya Kim <sonyakim.dev@gmail.com>
2026-05-22 15:43:30 -0700

    Add: word-break - submission-2

---
+++ Data Structures & Algorithms/word-break/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # DP - top-down
+        memo = {}
+
+        def dfs(s):
+            if s == "": return True
+            if s in memo: return memo[s]
+
+            for word in wordDict:
+                if s.startswith(word):
+                    if dfs(s[len(word):]):
+                        memo[s] = True
+                        return True
+            memo[s] = False
+            return False
+        
+        return dfs(s)
