19c8beaad33ce8a0f25ca5bdb028bee7e4f3f64b
Sonya Kim <sonyakim.dev@gmail.com>
2026-06-21 20:06:21 -0700

    Add: sqrtx - submission-0

---
+++ Data Structures & Algorithms/sqrtx/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l, r = 0, x
+        while l <= r:
+            m = (l + r) // 2
+            if m ** 2 == x:
+                return m
+
+            if m ** 2 > x:
+                r = m - 1
+            else:
+                l = m + 1
+        return l - 1
